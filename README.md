bb7c243196b7ac3cd8f457ac5c2928a25145bee3
Moritz Molch <moritz.molch@student.kit.edu>
2022-11-26 16:29:53 +0100

    Initial commit, basic law and luigi setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# htautau-sample-production
+
+Production of gg &#x2192; H &#x2192; &#x1D70F;&#x1D70F; samples with variable Higgs mass.
+
+The events are generated with `Pythia8` in leading order and interfaced with `FastSIM` for a fast simulation of the CMS detector. The workflow is managed with [law](https://github.com/riga/law).
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+action () {
+
+    # directory of that script and current working directory 
+    local base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    local current_dir="$( pwd )"
+
+    # production base paths
+    export PROD_BASE_PATH="${base_dir}"
+    export PROD_BASE_DATA_PATH="${PROD_BASE_PATH}/data"
+    export PROD_BASE_CONFIG_PATH="${PROD_BASE_PATH}/config"
+
+    # law and luigi paths
+    export LAW_HOME="${base_dir}/.law"
+    export LAW_CONFIG_FILE="${base_dir}/law.cfg"
+    export LUIGI_CONFIG_PATH="${base_dir}/luigi.cfg"
+
+}
+
+
+action "$@"
