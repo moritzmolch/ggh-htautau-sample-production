63125492f648581c6f4f22f76322217fdc555ac7
Moritz Molch <moritz.molch@student.kit.edu>
2022-12-17 10:25:39 +0100

    Rework setup

--- setup.sh
+++ setup.sh
@@ -12,16 +12,36 @@ action () {
     export PROD_CONFIG_PATH="${PROD_BASE_PATH}/config"
     export PROD_INPUTS_PATH="${PROD_BASE_PATH}/inputs"
 
+    # helpers
+    prod_pip_install () {
+        python3 -m pip install --ignore-installed --no-cache-dir --prefix "${PROD_BASE_PATH}/usr" "$@"
+    }
+    prod_add_py () {
+        if [[ ! -z "${1}" ]]; then
+            export PYTHONPATH="${1}:${PYTHONPATH}"
+        fi
+    }
+    prod_add_bin () {
+        if [[ ! -z "${1}" ]]; then
+            export PATH="${1}:${PATH}"
+        fi
+    }
+    prod_add_lib () {
+        if [[ ! -z "${1}" ]]; then
+            export LD_LIBRARY_PATH="${1}:${LD_LIBRARY_PATH}"
+        fi
+    }
+
     # set up CMSSW
     export VO_CMS_SW_DIR="/cvmfs/cms.cern.ch" 
     source "${VO_CMS_SW_DIR}/cmsset_default.sh" || return "$?"
     export CMSSW_VERSION="CMSSW_10_6_29_patch1"
-    export CMSSW_BASE_PATH="${PROD_BASE_PATH}/usr/${CMSSW_VERSION}"
+    export CMSSW_BASE_PATH="${PROD_BASE_PATH}/${CMSSW_VERSION}"
     export SCRAM_ARCH="slc7_amd64_gcc700"
 
     # CMSSW setup
     if [[ ! -d "${CMSSW_BASE_PATH}" ]]; then
-        cd "${PROD_BASE_PATH}/usr" || return "$?"
+        cd "${PROD_BASE_PATH}" || return "$?"
         scramv1 project CMSSW "${CMSSW_VERSION}" || return "$?"
         cd "${CMSSW_VERSION}/src" || return "$?"
         scramv1 build || return "$?"
@@ -34,20 +54,22 @@ action () {
     cd "${current_dir}" || return "$?"
 
     # install additional python packages
-    local python3_version="$( python3 -c "import sys;print('{0.major}.{0.minor}'.format(sys.version_info))" )"
-    _pip_install () {
-        python3 -m pip install --ignore-installed --no-cache-dir --prefix "${PROD_BASE_PATH}/usr" "$@"
-    }
-    _pip_install -r "${PROD_BASE_PATH}/requirements.txt"
+    prod_pip_install -r "${PROD_BASE_PATH}/requirements.txt"
 
     # law and luigi paths
-    export LAW_HOME="${base_dir}/.law"
-    export LAW_CONFIG_FILE="${base_dir}/law.cfg"
-    export LUIGI_CONFIG_PATH="${base_dir}/luigi.cfg"
+    export LAW_HOME="${PROD_BASE_PATH}/.law"
+    export LAW_CONFIG_FILE="${PROD_BASE_PATH}/law.cfg"
+    export LUIGI_CONFIG_PATH="${PROD_BASE_PATH}/luigi.cfg"
 
-    # add tasks as library to PYTHONPATH
-    export PYTHONPATH="${PROD_BASE_PATH}:${PROD_BASE_PATH}/usr/local/lib/python${python3_version}/site-packages:${PROD_BASE_PATH}/usr/local/lib/python${python3_version}/dist-packages:${PYTHONPATH}"
-    export PATH="${PROD_BASE_PATH}/usr/local/bin:${PATH}"
+    # add libraries and binaries to path variables
+    local python3_version="$( python3 -c "import sys;print('{0.major}.{0.minor}'.format(sys.version_info))" )"
+    prod_add_py "${PROD_BASE_PATH}/usr/local/lib/python${python3_version}/dist-packages"
+    prod_add_py "${PROD_BASE_PATH}/usr/local/lib/python${python3_version}/site-packages"
+    prod_add_py "${PROD_BASE_PATH}"
+    prod_add_bin "${PROD_BASE_PATH}/usr/local/bin"
+
+    # index tasks
+    python3 -m law index --verbose || return "$?"
 }
 
 
