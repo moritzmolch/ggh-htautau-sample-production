96e8facab089a497264aaa2eceb7e1cbf0f310b8
Moritz Molch <moritz.molch@student.kit.edu>
2022-12-22 20:11:19 +0100

    Add WLCG storage

--- production/framework.py
+++ production/framework.py
@@ -1,20 +1,39 @@
 import law
+import re
 import os
 import luigi
 import subprocess
 from abc import ABCMeta, abstractmethod
 
 
+law.contrib.load("wlcg")
+law.contrib.load("htcondor")
+law.contrib.load("git")
 
 
 class Task(law.Task):
 
+    wlcg_path = luigi.Parameter()
     local_data_path = luigi.Parameter()
     cmssw_path = luigi.Parameter()
 
+    _wlcg_file_systems = {}
 
     def __init__(self, *args, **kwargs):
         super(Task, self).__init__(*args, **kwargs)
+
+    @classmethod
+    def get_wlcg_file_system(cls, wlcg_path):
+        if wlcg_path not in cls._wlcg_file_systems:
+            cls._wlcg_file_systems[wlcg_path] = law.wlcg.WLCGFileSystem(None, base=wlcg_path)
+        return cls._wlcg_file_systems[wlcg_path]
+
+    def remote_path(self, *path):
+        parts = path
+        return os.path.join(*parts)
+
+    def remote_target(self, *path):
+        return law.wlcg.WLCGFileTarget(self.remote_path(*path), self.get_wlcg_file_system(self.wlcg_path))
 
     def local_path(self, *path):
         parts = (self.local_data_path, self.__class__.__name__) + path
@@ -195,3 +214,146 @@ class CMSDriverTask(law.Task):
         # build and execute the configuration generation command
         cmd = self.build_command()
         self.run_command(cmd, _output.parent.path)
+
+
+class HTCondorJobManager(law.contrib.htcondor.HTCondorJobManager):
+
+    status_line_cre = re.compile(r"^(\d+\.\d+)" + 4 * r"\s+[^\s]+" + r"\s+([UIRXSCHE<>])\s+.*$")
+
+    bootstrap_file = law.PathParameter(exists=True)
+
+    @classmethod
+    def get_htcondor_version(cls):
+        return (8, 6, 5)
+
+
+class HTCondorWorkflow(law.contrib.htcondor.HTCondorWorkflow):
+
+    prod_data_path = luigi.PathParameter(exists=True)
+    bootstrap_file = luigi.PathParameter(exists=True)
+
+    htcondor_universe = luigi.Parameter()
+    htcondor_docker_image = luigi.Parameter()
+
+    htcondor_request_cpus = luigi.IntParameter(default=1)
+    htcondor_request_gpus = luigi.IntParameter(default=0)
+    htcondor_request_memory = law.BytesParameter(default=2000, unit="MB")
+    htcondor_request_disk = law.BytesParameter(default=200000, unit="KB")
+    htcondor_request_walltime = law.DurationParameter(default=86400, unit="s")
+
+    htcondor_remote_job = luigi.BoolParameter(default=False)
+    htcondor_requirements = luigi.Parameter(default=law.NO_STR)
+
+    htcondor_accounting_group = luigi.Parameter(default=law.NO_STR)
+    htcondor_x509userproxy = law.wlcg.get_voms_proxy_file()
+
+    def htcondor_workflow_requires(self):
+        reqs = HTCondorWorkflow.htcondor_workflow_requires(self)
+        reqs["repo"] = BundleProductionRepository.req(self, replicas=3)
+        reqs["cmssw"] = BundleCMSSW.req(self, replicas=3)
+        return reqs
+
+    def htcondor_create_job_manager(self, **kwargs):
+        kwargs = law.util.merge_dicts(self.htcondor_job_manager_defaults, kwargs)
+        return HTCondorJobManager(**kwargs)
+
+    def htcondor_output_directory(self):
+        return law.LocalDirectoryTarget(os.path.join(self.prod_data_path, "condor_files"))
+
+    def htcondor_create_job_file_factory(self):
+        factory = super(HTCondorWorkflow, self).htcondor_create_job_file_factory()
+        factory.is_tmp = False
+        return factory
+
+    def htcondor_bootstrap_file(self):
+        return law.util.rel_path(__file__, self.bootstrap_file)
+
+    def htcondor_job_config(self, config, job_num, branches):
+
+        # custom config content
+        config.custom_content = []
+        config.custom_content.append(("universe", self.htcondor_universe))
+        config.custom_content.append(("docker_image", self.htcondor_docker_image))
+        config.custom_content.append(("request_cpus", self.htcondor_request_cpus))
+        config.custom_content.append(("request_gpus", self.htcondor_request_gpus))
+        config.custom_content.append(("request_memory", self.htcondor_request_memory))
+        config.custom_content.append(("request_disk", self.htcondor_request_disk))
+        config.custom_content.append(("+request_walltime", self.htcondor_request_walltime))
+        config.custom_content.append(("+remote_job", self.htcondor_remote_job))
+        config.custom_content.append(("requirements", self.htcondor_requirements))
+        config.custom_content.append(("accounting_group", self.htcondor_accounting_group))
+        config.custom_content.append(("x509userproxy", self.htcondor_x509userproxy))
+        return config
+
+    def htcondor_use_local_scheduler(self):
+        return True
+
+
+class BundleCMSSW(law.cms.BundleCMSSW, law.tasks.TransferLocalFile):
+
+    prod_bundle_path = law.PathParameter(exists=True)
+    replicas = luigi.Parameter()
+
+    task_namespace = None
+
+    exclude = "^src/tmp"
+
+    def get_cmssw_path(self):
+        return os.environ["PROD_CMSSW_BASE"]
+
+    def single_output(self):
+        filename = "{cmssw_version:s}-{checksum:s}.tar.gz".format(
+            cmssw_version=os.path.basename(self.get_cmssw_path()),
+            checksum=self.checksum,
+        )
+        return self.LocalFileTarget(os.path.join(self.prod_bundle_path, filename))
+
+    def get_file_pattern(self):
+        path = os.path.expandvars(os.path.expanduser(self.single_output().path))
+        return self.get_replicated_path(path, i=None if self.replicas <= 0 else "*")
+
+    def output(self):
+        return law.tasks.TransferLocalFile.output(self)
+
+    @law.decorator.safe_output
+    def run(self):
+        bundle = law.LocalFileTarget(is_tmp=True)
+        self.bundle(bundle)
+        self.publish_message(
+            "bundled CMSSW archive, size is {:.2f} {}".format(*law.util.human_bytes(bundle.stat().st_size))
+        )
+        self.transfer(bundle)
+
+
+class BundleProductionRepository(law.git.BundleGitRepository, law.tasks.TransferLocalFile):
+
+    prod_bundle_path = law.PathParameter(exists=True)
+
+    task_namespace = None
+
+    exclude_files = [".law", ".mypy", "config", "data", "venv", "*.pyc"]
+
+    def get_repo_path(self):
+        return os.environ["PROD_BASE_PATH"]
+
+    def single_output(self):
+        filename = "{repo_basename:s}-{checksum:s}.tar.gz".format(
+            repo_basename=os.path.basename(self.get_repo_path()), checksum=self.checksum
+        )
+        return self.LocalFileTarget(os.path.join(self.prod_bundle_path, filename))
+
+    def get_file_pattern(self):
+        path = os.path.expandvars(os.path.expanduser(self.single_output().path))
+        return self.get_replicated_path(path, i=None if self.replicas <= 0 else "*")
+
+    def output(self):
+        return law.tasks.TransferLocalFile.output(self)
+
+    @law.decorator.safe_output
+    def run(self):
+        bundle = law.LocalFileTarget(is_tmp=True)
+        self.bundle(bundle)
+        self.publish_message(
+            "bundled repository archive, size is {:.2f} {}".format(*law.util.human_bytes(bundle.stat().st_size))
+        )
+        self.transfer(bundle)
