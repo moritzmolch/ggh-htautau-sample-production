b4ca05d21132a7bcfaf6fc15173d2875f4fcd36f
Moritz Molch <moritz.molch@student.kit.edu>
2022-12-30 14:15:45 +0100

    Change setup of conda environment

---
+++ sandboxes/_setup_conda.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+
+setup_conda () {
+    # local variables pointing to some local paths
+    local shell_is_zsh this_file
+    shell_is_zsh="$( [[ -z "${ZSH_VERSION}" ]] && echo "false" || echo "true" )"
+    this_file="$( [[ "${shell_is_zsh}" == "true" ]] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]:-${0}}" )"
+
+    # local variables for conda installation
+    local miniconda_install_script_url="https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh"
+    local miniconda_install_script="miniconda_install_script.sh"
+    
+    local install_flag_file="${PROD_CONDA_BASE}/install"
+
+    # check environment variables
+    if [[ -z "${PROD_CONDA_BASE}" ]]; then
+        2>&1 echo "environment variable \$PROD_CONDA_BASE must not be empty (needed by script ${this_file})"
+    fi
+
+    # check if conda installation is necessary (check if installation flag file is already present in conda base directory)
+    local install_conda
+    install_conda="$( [[ -f "${install_flag_file}" ]] && echo "false" || echo "true" )"
+
+    # install 
+    if [[ "${install_conda}" == "true" ]]; then
+        # recreate conda base directory if it already exists 
+        if [[ -d "${PROD_CONDA_BASE}" ]]; then
+            rm -rf "${PROD_CONDA_BASE}" || return "$?"
+            mkdir -p "${PROD_CONDA_BASE}" || return "$?"
+        fi
+
+        # fetch install script and install miniconda
+        wget "${miniconda_install_script_url}" -O "${miniconda_install_script}" || return "$?"
+        bash "${miniconda_install_script}" -b -u -p "${PROD_CONDA_BASE}" || return "$?"
+        rm "miniconda_install_script.sh" || return "$?"
+        cat << EOF >> "${PROD_CONDA_BASE}/.condarc"
+changeps1: false
+channels:
+    - conda-forge
+    - defaults
+EOF
+    fi
+
+    source "${PROD_CONDA_BASE}/etc/profile.d/conda.sh" "" || return "$?"
+    conda activate || return "$?"
+
+    if [[ "${install_conda}" == "true" ]]; then
+        conda install --yes luigi law jinja2 || return "$?"
+
+        touch "${install_flag_file}"
+        echo "$( date +%s )" >> "${install_flag_file}"
+    fi
+
+    # prepend prioritized paths if the corresponding environment variables have been set
+    if [[ ! -z "${PROD_PREPEND_PATH}" ]]; then
+        export PATH="${PROD_PREPEND_PATH}:${PATH}"
+    fi
+    if [[ ! -z "${PROD_PREPEND_PYTHONPATH}" ]]; then
+        export PYTHONPATH="${PROD_PREPEND_PYTHONPATH}:${PYTHONPATH}"
+    fi
+
+}
+
+
+setup_conda "$@"
--- setup.sh
+++ setup.sh
@@ -1,40 +1,44 @@
 #!/usr/bin/env bash
 
-_setup_venv () {
-    local install_base="${1}"
-    local venv_name="${2}"
-    local requirements_file="${3}"
-
-    if [[ -d "${install_base}/${venv_name}" ]]; then
-        return
-    fi
-    python3 -m venv "${install_base}/${venv_name}" "" || ( rm -rf "${install_base}/${venv_name}" && return "$?" )
-    source "${install_base}/${venv_name}/bin/activate" || ( rm -rf "${install_base}/${venv_name}" && return "$?" )
-    python3 -m pip install -U pip  || ( rm -rf "${install_base}/${venv_name}" && return "$?" )
-    python3 -m pip install -r "${requirements_file}" || ( rm -rf "${install_base}/${venv_name}" && return "$?" )
-    deactivate || return "$?"
-}
 
 action () {
     # directory of that script and current working directory
-    local base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    local current_dir="$( pwd )"
+    local shell_is_zsh this_file this_dir
+    shell_is_zsh="$( [[ -z "${ZSH_VERSION}" ]] && echo "false" || echo "true" )"
+    this_file="$( [[ "${shell_is_zsh}" == "true" ]] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]:-${0}}" )"
+    this_dir="$( cd "$( dirname "${this_file}" )" && pwd )"
 
     # set project base paths
-    export PROD_BASE="${base_dir}"
+    export PROD_BASE="${this_dir}"
     export PROD_SOFTWARE_BASE="${PROD_BASE}/software"
+    export PROD_CONDA_BASE="${PROD_SOFTWARE_BASE}/conda"
     export PROD_VENV_BASE="${PROD_SOFTWARE_BASE}/venvs"
 
-    _setup_venv "${PROD_VENV_BASE}" "venv_base" "${PROD_BASE}/requirements.txt" || return "$?"
-    source "${PROD_VENV_BASE}/venv_base/bin/activate" "" || return "$?"
-    export PYTHONPATH="${PROD_BASE}:${PYTHONPATH}"
+    # save original paths for binaries and libraries
+    export PROD_ORIG_PATH="${PATH}"
+    export PROD_ORIG_LD_LIBRARY_PATH="${LD_LIBRARY_PATH}"
+    export PROD_ORIG_PYTHONPATH="${PYTHONPATH}"
+    export PROD_ORIG_PYTHON3PATH="${PYTHON3PATH}"
+
+    # define persistent parts of PATH and PYTHONPATH that should always be prepended to the effective PATH and
+    # PYTHONPATH of the current environment
+    # these parts contain special binaries and libraries that should always be prioritized, e.g. the
+    # 'production/' directory that contains the task definitions
+    export PROD_PREPEND_PATH="${PROD_SOFTWARE_BASE}/local/bin"
+    export PROD_PREPEND_PYTHONPATH="${PROD_SOFTWARE_BASE}"
+
+    # provide python environment with conda
+    source "${this_dir}/sandboxes/_setup_conda.sh" "" || return "$?"
 
     # law and luigi paths
     export LAW_HOME="${PROD_BASE}/.law"
     export LAW_CONFIG_FILE="${PROD_BASE}/law.cfg"
     export LUIGI_CONFIG_PATH="${PROD_BASE}/luigi.cfg"
 
-    law index -q
+    if [[ "$( which law > /dev/null )" && "$?" == "0" ]]; then
+        source "$( law completion )" || return "$?"
+        law index -q
+    fi
 
 }
 
