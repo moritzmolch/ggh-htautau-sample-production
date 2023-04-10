4929707f191129cdfe1586024c1035251d8a08f8
Moritz Molch <moritz.molch@student.kit.edu>
2023-04-10 20:41:46 +0200

    Fix some bugs in fragment task

--- production/tasks/fragment.py
+++ production/tasks/fragment.py
@@ -1,11 +1,15 @@
 from jinja2 import Template
 import law
+import luigi
 import os
 
-from production.tasks.base import ProcessTask
+from production.tasks.base import BaseTask
 
 
-class FragmentGeneration(ProcessTask, law.LocalWorkflow):
+class FragmentGeneration(BaseTask):
+
+    higgs_mass = luigi.IntParameter(description="mass of the Higgs boson that is put into the event generator configuration")
+
     cmssw_path = os.path.expandvars("${PROD_CMSSW_BASE}")
     fragment_template = os.path.abspath(
         os.path.join(
@@ -19,38 +23,35 @@ class FragmentGeneration(ProcessTask, law.LocalWorkflow):
     exclude_params_req_get = {"workflow"}
     prefer_params_cli = {"workflow"}
 
-    def create_branch_map(self):
-        branch_map = super(FragmentGeneration, self).create_branch_map()
-        return branch_map
+    def get_fragment_filename(self, higgs_mass):
+        return "GluGluHToTauTau_MH{higgs_mass:d}_pythia8_TuneCP5_cff.py".format(higgs_mass=higgs_mass)
 
     def output(self):
-        _process_inst = self.branch_data["process_inst"]
+        _higgs_mass = self.branch_data["higgs_mass"]
         return self.local_target(
             "src",
             "Configuration",
             "GenProduction",
             "python",
-            "{filename_prefix:s}_cff.py".format(
-                filename_prefix=_process_inst.get_aux("filename_prefix")
-            ),
+            self.get_fragment_filename(_higgs_mass),
             store=self.cmssw_path,
         )
 
     def run(self):
         # get the output and the process instance of this branch
         _output = self.output()
-        _process_inst = self.branch_data["process_inst"]
+        _higgs_mass = self.higgs_mass
 
         # load template and replace placeholders
         _fragment_template = law.LocalFileTarget(self.fragment_template)
         with _fragment_template.open(mode="r") as f:
             template = Template(f.read())
-        content = template.render(higgs_mass=float(_process_inst.get_aux("higgs_mass")))
+        content = template.render(higgs_mass=_higgs_mass)
 
         # write the fragment content to the output target
         _output.dump(content, formatter="text")
         self.publish_message(
-            "successfully created fragment for process {process:s}".format(
-                process=_process_inst.name
+            "successfully created fragment for dataset with generator Higgs mass of {higgs_mass:s} GeV".format(
+                higgs_mass=str(_higgs_mass)
             )
         )
